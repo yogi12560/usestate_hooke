3511b15ae1d4541cd2ed385f1b99fe9e93ca514b
yogi12560 <49420227+yogi12560@users.noreply.github.com>
2023-08-01 23:18:42 +0530

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
 import './style.css';
+import { useState } from 'react';
 
 export default function App() {
+  const initial = 1;
+  const [state, setState] = useState(initial);
+
+  const Addition = () => {
+      setState(state+1);
+  };
+
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <button onClick={Addition}> ADD </button>
+
+      <p> {state} </p>
     </div>
   );
 }
